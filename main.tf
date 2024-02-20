974c87aa0e35e3668d10ab7df784e8c95cc45a14
jw-tera <juan.wiggenhauser@teracloud.io>
2024-02-20 11:24:24 -0300

    feat: adding source code for versioning

--- main.tf
+++ main.tf
@@ -18,7 +18,12 @@ locals {
   s3_bucket         = var.s3_existing_package != null ? try(var.s3_existing_package.bucket, null) : (var.store_on_s3 ? var.s3_bucket : null)
   s3_key            = var.s3_existing_package != null ? try(var.s3_existing_package.key, null) : (var.store_on_s3 ? var.s3_prefix != null ? format("%s%s", var.s3_prefix, replace(local.archive_filename_string, "/^.*//", "")) : replace(local.archive_filename_string, "/^\\.//", "") : null)
   s3_object_version = var.s3_existing_package != null ? try(var.s3_existing_package.version_id, null) : (var.store_on_s3 ? try(aws_s3_object.lambda_package[0].version_id, null) : null)
+  source_code_hash  = data.aws_lambda_function.existing.source_code_hash
 
+}
+
+data "aws_lambda_function" "existing" {
+  function_name = var.function_name
 }
 
 resource "aws_lambda_function" "this" {
@@ -50,7 +55,7 @@ resource "aws_lambda_function" "this" {
   }
 
   filename         = local.filename
-  source_code_hash = var.ignore_source_code_hash ? null : (local.filename == null ? false : fileexists(local.filename)) && !local.was_missing ? filebase64sha256(local.filename) : null
+  source_code_hash = var.ignore_source_code_hash ? null : var.s3_existing_package == null ? local.source_code_hash : (local.filename == null ? false : fileexists(local.filename)) && !local.was_missing ? filebase64sha256(local.filename) : null
 
   s3_bucket         = local.s3_bucket
   s3_key            = local.s3_key
